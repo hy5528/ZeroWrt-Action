c635d940100d4ccc1e1cb7772e68e4db6b2823b1
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-04-02 22:09:34 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -82,7 +82,7 @@ sed -i "s?/bin/login?/usr/libexec/login.sh?g" ${GITHUB_WORKSPACE}/openwrt/packag
 sed -i 's/CONFIG_PACKAGE_luci-theme-bootstrap=y/CONFIG_PACKAGE_luci-theme-bootstrap=n/' .config
 
 # 添加第三方软件源
-git clone --depth=1 https://github.com/kiddin9/kwrt-packages package/openwrt-package
+git clone --depth=1 https://github.com/kiddin9/kwrt-packages package/kwrt-packages
 
 # 添加第三方应用
 mkdir kiddin9
